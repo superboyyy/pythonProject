3783925f7eb97bf489fb1278605b68d4842879ac
szsx4 <1282635227@qq.com>
2021-02-03 19:29:59 +0800

    change

--- function/8-3.t_shirt.py
+++ function/8-3.t_shirt.py
@@ -1,2 +1,6 @@
 def make_shirt(size, sample):
-    print()
+    print(f"The t-shirt's size is {size} and its sample is {sample}.")
+
+make_shirt('6', 'happy')
+
+make_shirt(size='6', sample='happy')
